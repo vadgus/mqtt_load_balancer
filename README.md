fd02e2e73fddf88b11069791d0df8d97776d74a6
Vadim Gusyatnikov <guva@gurtam.com>
2021-07-30 17:03:11 +0300

    project base scructure

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # load_balancer
+
+balancer.py runs several worker.py processes using balancer.ini config
+
+### Setup env
+
+```bash
+python3 -m venv env
+source env/bin/activate
+pip3 install -r requirements.txt
+```
---
+++ worker.py
@@ -0,0 +1,75 @@
+import asyncio
+import sys
+import signal
+import time
+import uvloop
+
+from configparser import ConfigParser
+
+from gmqtt import Client as MQTTClient
+
+asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+
+STOP = asyncio.Event()
+
+
+def on_connect(client, flags, rc, properties):
+    print('Connected')
+    client.subscribe('TEST/#', qos=0)
+
+
+def on_message(client, topic, payload, qos, properties):
+    print('RECV MSG:', payload)
+
+
+def on_disconnect(client, packet, exc=None):
+    print('Disconnected')
+
+
+def on_subscribe(client, mid, qos, properties):
+    print('SUBSCRIBED')
+
+
+def ask_exit(*args):
+    STOP.set()
+
+
+async def main(config):
+    worker_index = ''
+    args = sys.argv[1:]
+    for arg in args:
+        if arg.startswith('--index='):
+            worker_index = arg[8:]
+    if not worker_index:
+        ask_exit()
+        raise Exception("process argument '--index=' invalid")
+
+    client_id = f'client_id_{worker_index}'
+    client = MQTTClient(client_id)
+
+    client.on_connect = on_connect
+    client.on_message = on_message
+    client.on_disconnect = on_disconnect
+    client.on_subscribe = on_subscribe
+
+    client.set_auth_credentials(config.get('FLESPI', 'TOKEN'), None)
+    await client.connect(config.get('FLESPI', 'HOST'))
+
+    client.publish(f'debug/{worker_index}', str(time.time()), qos=1)
+
+    await STOP.wait()
+    await client.disconnect()
+
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+
+    host = 'mqtt.flespi.io'
+
+    config = ConfigParser()
+    config.read('.env')
+
+    loop.add_signal_handler(signal.SIGINT, ask_exit)
+    loop.add_signal_handler(signal.SIGTERM, ask_exit)
+
+    loop.run_until_complete(main(config))
