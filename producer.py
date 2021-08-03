89008c4cf39bb86f674c938191708f102af8a8ad
Vadim Gusyatnikov <guva@gurtam.com>
2021-08-03 15:01:14 +0300

    producer refactoring

--- producer.py
+++ producer.py
@@ -6,20 +6,18 @@ from gmqtt import Client as MQTTClient
 from configparser import ConfigParser
 
 
-def pub(client, item_id):
-    client.publish(f'debug/balancer/{item_id}', str(time.time()))
+async def main():
+    config = ConfigParser()
+    config.read('.env')
 
-
-async def run(config):
-    client_id = f'client_id_producer'
-    client = MQTTClient(client_id)
+    client = MQTTClient('producer')
     client.set_auth_credentials(config.get('FLESPI', 'TOKEN'), None)
     await client.connect(config.get('FLESPI', 'HOST'))
 
+    c = 0
+    t = time.time()
     messages_per_sec = int(config.get('PRODUCER', 'MESSAGES_PER_SEC') or 1)
 
-    t = time.time()
-    c = 0
     while True:
         if c >= messages_per_sec:
             if time.time() - t >= 1:
@@ -27,13 +25,11 @@ async def run(config):
                 c = 0
             continue
 
-        item_id = random.randint(1, 5)
-        pub(client, item_id)
+        item_id = random.randint(1, 2000000)
+        client.publish(f'debug/balancer/{item_id}', str(time.time()))
         c += 1
 
 
-config = ConfigParser()
-config.read('.env')
 loop = asyncio.get_event_loop()
-future = asyncio.ensure_future(run(config))
+future = asyncio.ensure_future(main())
 loop.run_until_complete(future)
